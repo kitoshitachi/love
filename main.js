aa7235adc118eee342c84d8187b7a0cc47546405
kitoshitachi <19521204@gm.uit.edu.vn>
2022-11-12 16:55:00 +0700

    hover active button, mouvemove event

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict";
 
+
+document.addEventListener("mousemove",function(e){
+    var body = document.querySelector("body");
+    var heart = document.createElement("span");
+    var x = e.offsetX;
+    var y = e.offsetY;
+    heart.style.left = x + 'px';
+    heart.style.top = y + 'px';
+    
+
+    body.appendChild(heart);
+
+})
